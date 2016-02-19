e650277df58a8dbe5936249a9895fb4b93667108
PDDaniel <daniel@proper-dog.com>
2016-02-19 10:55:13 +0100

    Save1

---
+++ Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    public GameObject test1;
+    public GameObject test2;
+    public GameObject test3;
+    public GameObject test4;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
